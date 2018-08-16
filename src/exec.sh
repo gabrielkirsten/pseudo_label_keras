d489cf9d8005d28a08f27932d5d7413cbd828a80
Gabriel Kirsten Menezes <gabriel.kirsten@hotmail.com>
2018-08-16 09:57:50 -0400

    removed invalid values

--- src/exec.sh
+++ src/exec.sh
@@ -1,5 +1,5 @@
 for i in Xception VGG16 VGG19 ResNet50 InceptionV3; do
-	for j in -1 20 20 30 40 50 60 70 80; do
+	for j in 20 20 30 40 50 60 70 80; do
 		nohup python main.py -d /home/aluno/gabriel/_dataset/doencas/doencas3000/ -n $j -a $i 
 		# mv ../models_checkpoints/* /media/hd/gabriel/experimento10oct/
 	done
