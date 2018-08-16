1b60d640b8826bc3f66f76cbf4a74128e22fa4d4
Gabriel Kirsten Menezes <gabriel.kirsten@hotmail.com>
2018-08-16 10:06:40 -0400

    do not execute in parallel

--- src/exec.sh
+++ src/exec.sh
@@ -1,6 +1,6 @@
 for i in Xception VGG16 VGG19 ResNet50 InceptionV3; do
 	for j in 20 20 30 40 50 60 70 80; do
-		nohup python main.py -d /home/aluno/gabriel/_dataset/doencas/doencas3000 -n $j -a $i  > ../results/"$i""_""$j".txt &
+		python main.py -d /home/aluno/gabriel/_dataset/doencas/doencas3000 -n $j -a $i  > ../results/"$i""_""$j".txt
 		# mv ../models_checkpoints/* /media/hd/gabriel/experimento10oct/
 	done
 done
