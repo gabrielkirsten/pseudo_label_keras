57973d0490bc265cb12afec346312d6038dec373
Gabriel Kirsten Menezes <gabriel.kirsten@hotmail.com>
2018-09-12 22:22:29 -0400

    implemented learning curve

--- src/metrics/LearningCurve.py
+++ src/metrics/LearningCurve.py
@@ -1,5 +1,7 @@
-from metrics.Metric import Metric
+import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
+
+from metrics.Metric import Metric
 
 
 class LearningCurve(Metric):
@@ -7,6 +9,20 @@ class LearningCurve(Metric):
         pass
 
     @staticmethod
-    def obtain(self, output_predict, output_real):
-        accuracy = accuracy_score(output_real, output_predict)
+    def obtain(data_points_to_learning_curve, plot_graphic=False):
+        data_points_qtd_examples = []
+        data_points_accuracy = []
+        for data_point in data_points_to_learning_curve:
+            accuracy = accuracy_score(data_point.get('output_real'), data_point.get('output_predict'))
+            data_points_qtd_examples.append(data_point.get('qtd_examples'))
+            data_points_accuracy.append(accuracy)
+
+            if not plot_graphic:
+                print("Qtd examples %d - Accuracy: %f" % (data_point.get('qtd_examples'), accuracy))
+        
+        if plot_graphic:
+            plt.figure(1)
+            plt.plot(data_points_qtd_examples, data_points_accuracy, 'ob-')
+            plt.show()
+            
         
